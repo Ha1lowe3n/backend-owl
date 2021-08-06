123a694322ad791efcdfccead8667fdac312498d
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-08-06 11:29:37 +0800

    connection models

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,11 @@
-export class AuthModel {
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export interface AuthModel extends Base {} // lifehack for AuthModel extends Base too
+export class AuthModel extends TimeStamps {
+    @prop({ unique: true }) // unique - поле должно быть уникальным
     email: string;
+
+    @prop()
     passwordHash: string;
 }
--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
 export enum TopLevelCategory {
     Courses,
     Services,
@@ -5,23 +8,57 @@ export enum TopLevelCategory {
     Products,
 }
 
-export class TopPageModel {
-    _id: string;
-    firstCategory: TopLevelCategory;
-    secondCategory: string;
+class HhData {
+    @prop()
+    count: number;
+
+    @prop()
+    juniorSalary: number;
+
+    @prop()
+    middleSalary: number;
+
+    @prop()
+    seniorSalary: number;
+}
+
+class Advantage {
+    @prop()
     title: string;
+
+    @prop()
+    description: string;
+}
+
+export interface TopPageModel extends Base {}
+export class TopPageModel extends TimeStamps {
+    @prop({ enum: TopLevelCategory })
+    firstCategory: TopLevelCategory;
+
+    @prop()
+    secondCategory: string;
+
+    @prop({ unique: true })
+    alias: string;
+
+    @prop()
+    title: string;
+
+    @prop()
     category: string;
-    hhru?: {
-        count: number;
-        juniorSalary: number;
-        middleSalary: number;
-        seniorSalary: number;
-    };
-    advantages: {
-        title: string;
-        description: string;
-    }[];
+
+    @prop({ type: () => HhData })
+    hhru?: HhData;
+
+    @prop({ type: () => [Advantage] })
+    advantages: Advantage[];
+
+    @prop()
     seoText: string;
+
+    @prop()
     tagsTitle: string;
+
+    @prop({ type: () => [String] })
     tags: string[];
 }
