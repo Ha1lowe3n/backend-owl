ab9a98fc39178f8e659c9188fb746cdb6b38f9ac
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-08-09 11:21:07 +0800

    add login and jwt for auth

--- src/auth/auth.constans.ts
+++ src/auth/auth.constans.ts
@@ -1,2 +1,4 @@
 export const ALREADY_REGISTER_ERROR =
     'Пользователь с таким email уже существует';
+export const USER_NOT_FOUND_ERROR = 'Пользователь c таким email не найден';
+export const WRONG_PASSWORD_ERROR = 'Неверный пароль';
