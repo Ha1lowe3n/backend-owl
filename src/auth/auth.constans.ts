8ffb8770008ccadb9df51d4a31dc2fd5e930682b
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-11-10 00:22:56 +0800

    clear project, start again

--- src/auth/auth.constans.ts
+++
@@ -1,4 +0,0 @@
-export const ALREADY_REGISTER_ERROR =
-    'Пользователь с таким email уже существует';
-export const USER_NOT_FOUND_ERROR = 'Пользователь c таким email не найден';
-export const WRONG_PASSWORD_ERROR = 'Неверный пароль';
