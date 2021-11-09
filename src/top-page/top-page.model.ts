8f0f64623c220cff202d5cd6707a14f98e91bccd
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-11-10 01:14:00 +0800

    add models

---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,25 @@
+export enum TopLevelCategory {
+    Courses,
+    Services,
+    Books,
+    Products,
+}
+
+export class TopPageModel {
+    firstCategory: TopLevelCategory;
+    secondCategory: string;
+    title: string;
+    categoty: string;
+    hh?: {
+        count: number;
+        juniorSalary: number;
+        middleSalary: number;
+        seniorSalary: number;
+    };
+    advantages: {
+        title: string;
+        description: string;
+    }[];
+    seoText: string;
+    tagsSkills: string[];
+}
